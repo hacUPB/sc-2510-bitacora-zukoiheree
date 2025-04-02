12d3638367ac9af15102f6a705ee09353e994b95
Valentina <thereader200512@gmail.com>
2025-04-02 15:38:17 -0500

    Update prueba.asm

--- Unidad 1/prueba.asm
+++ Unidad 1/prueba.asm
@@ -1,83 +1,222 @@
-// Inicialización
-@0
+(INICIO)
+@SCREEN  // Dirección base de la pantalla
+
+@16384   // Dirección inicial de SCREEN
 D=A
-@tecla_presionada
-M=D // Variable para indicar si se presionó la tecla "d"
 
-// Bucle principal para verificar tecla
-(LOOP_TECLA)
-    @24576 // Dirección del teclado
-    D=M
-    @100 // Valor ASCII de "d"
-    D=D-A
-    @DIBUJAR // Si se presionó "d", salta a dibujar
-    D;JEQ
+@16      // Guardar la dirección de inicio en R16
+M=D
 
-    @LOOP_TECLA // Si no se presionó "d", vuelve a verificar
-    0;JMP
+@24576   // Dirección del teclado (KBD)
+D=M      // Leer la tecla presionada
+@100     // Código ASCII de 'd'
+D=A
+@24576   // Leer nuevamente el valor del teclado
+D = M - D // Comparar con 'd'
+@Bitmap  // Si es 'd', saltar a dibujar el bitmap
+D;JEQ 
+@INICIO  // Si no es 'd', reiniciar el loop
+0;JMP
 
-// Rutina para dibujar el bitmap
-(DIBUJAR)
-    // Asumiendo que el bitmap está diseñado en las direcciones
-    // que se modifican a continuación. Ajusta según sea necesario.
-    @16384 // Dirección de la pantalla
-    D=A
-    @12
-    AD=D+M
-    @8
-    D=D+A
-    A=D-A
-    M=D-A
-
-    D=A
-    @32
-    AD=D+A
-    @16404
-    D=D+A
-    A=D-A
-    M=D-A
-
-    AD=A+1
-    @2048
-    D=D+A
-    A=D-A
-    M=D-A
-
-    AD=A+1
-    @2
-    D=D+A
-    A=D-A
-    M=D-A
-
-    D=A
-    @30
-    AD=D+A
-    @8201
-    D=D+A
-    A=D-A
-    M=D-A
-
-    AD=A+1
-    @4096
-    D=D+A
-    A=D-A
-    M=D-A
-
-    AD=A+1
-    @33
-    D=D+A
-    A=D-A
-    M=D-A
-
-    D=A
-    @30
-    AD=D+A
-    @5600
-    D=D+A
-    A=D-A
-    M=D-A
-
-    @LOOP_TECLA // Vuelve al bucle principal
-    0;JMP
-
+(Bitmap)
+  D=A
+  @R12
+  AD=D+M
+  M=-1  // Dibujar pixel encendido
+  AD=A+1
+  @7
+  D=D+A
+  A=D-A
+  M=D-A
+  
+  D=A
+  @31
+  AD=D+A
+  M=1
+  AD=A+1
+  @4
+  D=D+A
+  A=D-A
+  M=D-A
+  
+  D=A
+  @31
+  AD=D+A
+  M=1
+  AD=A+1
+  @4
+  D=D+A
+  A=D-A
+  M=D-A
+  
+  D=A
+  @31
+  AD=D+A
+  M=1
+  AD=A+1
+  @4
+  D=D+A
+  A=D-A
+  M=D-A
+  
+  D=A
+  @31
+  AD=D+A
+  M=1
+  AD=A+1
+  @4
+  D=D+A
+  A=D-A
+  M=D-A
+  
+  D=A
+  @31
+  AD=D+A
+  M=1
+  AD=A+1
+  @4
+  D=D+A
+  A=D-A
+  M=D-A
+  
+  D=A
+  @31
+  AD=D+A
+  @3971
+  D=D+A
+  A=D-A
+  M=A-D
+  AD=A+1
+  @5
+  D=D+A
+  A=D-A
+  M=D-A
+  
+  D=A
+  @31
+  AD=D+A
+  @3971
+  D=D+A
+  A=D-A
+  M=A-D
+  AD=A+1
+  @5
+  D=D+A
+  A=D-A
+  M=D-A
+  
+  D=A
+  @31
+  AD=D+A
+  @24535
+  D=D+A
+  A=D-A
+  M=A-D
+  AD=A+1
+  @4
+  D=D+A
+  A=D-A
+  M=D-A
+  
+  D=A
+  @31
+  AD=D+A
+  @24535
+  D=D+A
+  A=D-A
+  M=A-D
+  AD=A+1
+  @4
+  D=D+A
+  A=D-A
+  M=D-A
+  
+  D=A
+  @31
+  AD=D+A
+  @24529
+  D=D+A
+  A=D-A
+  M=A-D
+  AD=A+1
+  @7
+  D=D+A
+  A=D-A
+  M=D-A
+  
+  D=A
+  @31
+  AD=D+A
+  @24536
+  D=D+A
+  A=D-A
+  M=A-D
+  
+  D=A
+  @32
+  AD=D+A
+  @24536
+  D=D+A
+  A=D-A
+  M=A-D
+  
+  D=A
+  @32
+  AD=D+A
+  @24536
+  D=D+A
+  A=D-A
+  M=A-D
+  
+  D=A
+  @32
+  AD=D+A
+  @24536
+  D=D+A
+  A=D-A
+  M=A-D
+  
+  D=A
+  @32
+  AD=D+A
+  @24536
+  D=D+A
+  A=D-A
+  M=A-D
+  
+  D=A
+  @32
+  AD=D+A
+  @24536
+  D=D+A
+  A=D-A
+  M=A-D
+  
+  D=A
+  @32
+  AD=D+A
+  @20568
+  D=D+A
+  A=D-A
+  M=A-D
+  
+  D=A
+  @32
+  AD=D+A
+  @22360
+  D=D+A
+  A=D-A
+  M=A-D
+  
+  D=A
+  @32
+  AD=D+A
+  @1800
+  D=D+A
+  A=D-A
+  M=A-D
+  
+  @R13
+  A=M
+  D;JMP
 
